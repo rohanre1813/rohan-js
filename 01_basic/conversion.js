dd33207bb6d397588ac21f22f5898808cb2aad36
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-05 18:18:39 +0000

    config done

---
+++ 01_basic/conversion.js
@@ -0,0 +1,7 @@
+console.log('rohan');
+
+console.log(1 + "2");
+console.log('1' + 2 +3);
+console.log(1+2+'3');
+
+
--- 01_basic/ts.js
+++ 01_basic/ts.js
@@ -1,1 +1,17 @@
  console.log("rohan");
+ const accountID=14453
+ let accountemail = "rohan@gmail.com"
+ var accountpsswd = "135781"
+ accountcity = "banglore"
+  console.log(accountemail);
+  accountemail="roh@gmail,com"
+  accountpsswd = "12345"
+  accountcity = "jamshedpur"
+ let no="33"
+ let value= Number(no)
+ console.log(value);
+
+  console.table([accountID ,accountemail , accountpsswd ,accountcity])
+// "33"=>33
+// "33avs"=>NaN(NOT A NUMBER)
+// true=> 1
