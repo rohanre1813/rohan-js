5367d17b538236204fc0d6ea07e3df9c52ee40b6
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-11 04:42:15 +0000

    arrays in js

---
+++ 01_basic/array.js
@@ -0,0 +1,10 @@
+// array in javascript
+const a=[1,2,3,4,5,6,7] //declaration
+console.log(a);
+const ab= new Array(1,2,3,4,5,6)//also declare an array like this
+console.log(ab);
+// methods of arrays in javascript
+ab.push(7)
+ab.push(8)
+ab.pop()
+console.log(ab);
