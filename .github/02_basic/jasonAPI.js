98e4b1d4fe4fa3c195f20ca79fa2155ab75956fd
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-11 04:42:55 +0000

    jasonAPI

---
+++ .github/02_basic/jasonAPI.js
@@ -0,0 +1,17 @@
+const company={
+     name: "infosys",
+     manpower: 2000,
+     location: "banglore"
+}
+const {name}=company //dont have to write company.name after this
+console.log(name);
+// {
+
+// } this is jasonAPI where data will be stored in form of objects ,array 
+// where keys of object are also represented as string
+
+[
+    {},
+    {},
+    {}
+]// this is also JsonAPI
