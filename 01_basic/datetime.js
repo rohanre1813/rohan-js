3d37985c689ff7451f09ba781e5474765e42fff1
rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-04-11 04:43:43 +0000

    date and time

---
+++ 01_basic/datetime.js
@@ -0,0 +1,12 @@
+let mydate=new Date()
+console.log(mydate);
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toTimeString());
+console.log(mydate.toLocaleString());
+console.log(mydate.getTimezoneOffset());
+let mdate=new Date(2023,2,12)
+console.log(mdate.toDateString());
+
+let ddate= Date.now()
+console.log(ddate);
